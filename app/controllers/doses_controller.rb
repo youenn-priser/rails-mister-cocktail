82da4febd08e92aeefa1bc5a6a68c04426da6b1c
youenn priser <youenn.priser@gmail.com>
2020-02-28 11:57:33 +0100

    Created route to home page

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,22 @@
+class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
